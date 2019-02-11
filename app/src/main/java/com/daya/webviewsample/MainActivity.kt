fe1297421908fb51e24ec4ee2525cbf811f21214
shnoble <shnoble79@gmail.com>
2019-02-12 00:38:04 +0900

    Implement webview loading progress bar

--- app/src/main/java/com/daya/webviewsample/MainActivity.kt
+++ app/src/main/java/com/daya/webviewsample/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.daya.webviewsample
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadWebPage()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun loadWebPage() {
+        webView.webViewClient = ToastWebViewClient()
+        webView.webChromeClient = ToastWebChromeClient()
+        webView.settings.javaScriptEnabled = true
+        //webView.loadUrl("https://m.naver.com")
+        webView.loadUrl("https://toast.com")
+    }
+
+    inner class ToastWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            loadingProgressBar.visibility = View.VISIBLE
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            loadingProgressBar.visibility = View.GONE
+        }
+    }
+
+    inner class ToastWebChromeClient : WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            loadingProgressBar.progress = newProgress
+        }
     }
 }
